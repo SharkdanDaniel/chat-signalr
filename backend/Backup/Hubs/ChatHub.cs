44695423889332f152fbabf8f3a2e3d3b46c22aa
Daniel <sharkdan_daniel@hotmail.com>
2021-03-18 13:02:41 -0300

    atualizando arquivos

---
+++ backend/Backup/Hubs/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Hubs
+{
+    public class ChatHub : Hub<IChatHub>
+    {
+        ILogger _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+           var connectionId = Context.ConnectionId;
+            _logger.LogInformation($"{connectionId} Está conectado");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var connectionId = Context.ConnectionId;
+            await Groups.RemoveFromGroupAsync(connectionId, "room");
+            _logger.LogInformation($"{connectionId} desconectado");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        [HubMethodName("SendMessage")]
+        public async Task SendMessage(string message)
+        {
+            await this.Clients.AllExcept(this.Context.ConnectionId).ReceiveMessage(message);
+            _logger.LogInformation($"Mensagem enviada com sucesso: {message}");
+        }
+    }
+}
---
+++ backend/Backup/Hubs/IChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Hubs
+{
+    public interface IChatHub
+    {
+        Task ReceiveMessage(string message);
+        Task SendMessage(string message);
+        Task OnConnect(string message);
+    }
+}
